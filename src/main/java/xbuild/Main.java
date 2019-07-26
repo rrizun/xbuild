0bd5816aa5ed34a8053fba0cf21bf8386926b20d
rrizun <rrizun@gmail.com>
2019-07-26 04:04:33 -0700

    checkpoint: archive

--- src/main/java/xbuild/Main.java
+++ src/main/java/xbuild/Main.java
@@ -1,16 +1,21 @@
 package xbuild;
 
-import java.io.File;
-import java.lang.ProcessBuilder.Redirect;
+import java.io.*;
+import java.nio.file.*;
+import java.time.*;
+import java.util.*;
+import java.util.regex.*;
+import java.util.zip.*;
 
-import com.google.common.collect.Lists;
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.archive.*;
+import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.storage.file.*;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.google.common.base.*;
+import com.google.common.collect.*;
 
 /**
  * https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle
@@ -24,7 +29,11 @@ public class Main implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		log("run");
+
+		log("run", args);
+
+		// e.g., 20191231235959
+		final String timestamp = CharMatcher.anyOf("0123456789").retainFrom(Instant.now().toString()).substring(0,14);
 
 		log("optionNames", args.getOptionNames());
 		log("nonOptionArgs", args.getNonOptionArgs());
@@ -36,19 +45,123 @@ public class Main implements ApplicationRunner {
 				// .findGitDir() // scan up the file system tree
 				.build();
 
-		log(repository.getBranch());
+		String branch = repository.getBranch();
 
-		run("./xbuildfile");
+		Map<Integer, String> allTags = Maps.newTreeMap();
+		Map<Integer, String> branchTags = Maps.newTreeMap();
+
+		for (Ref ref : repository.getRefDatabase().getRefsByPrefix(Constants.R_TAGS)) {
+			String tag = new File(ref.getName()).getName();
+			log(tag);
+
+			if (tag.contains("xbuild")) {
+
+				int num = Integer.parseInt(Iterables.getLast(findall("[0-9]+", tag)));
+				allTags.put(num, tag);
+
+			}
+
+			
+		}
+
+		int buildNumber = 0;
+		if (allTags.size()>0)
+			buildNumber = Iterables.getLast(allTags.keySet());
+
+		int buildNumberNext = ++buildNumber;
+
+		log("buildNumberNext", buildNumberNext);
+
+		String newTag = String.format("xbuild-%s-%s-%s", branch, buildNumberNext, timestamp);
+		log("newTag", newTag);
+
+		Map<String, String> env = Maps.newHashMap();
+		env.put("XBUILD_BRANCH", branch);
+		env.put("XBUILD_NUMBER", ""+buildNumberNext);
+		env.put("XBUILD_TIMESTAMP", timestamp);
+
+		Path tmp = Files.createTempDirectory("xbuild");
+
+		log(tmp);
+
+		Path tempFile = Files.createTempFile("xbuild", ".zip");
+		log(tempFile);
+		
+		final OutputStream out = Files.newOutputStream(tempFile);
+		
+		ArchiveFormats.registerAll();
+		
+//    ArchiveCommand.registerFormat("zip", null);
+    
+    try (Git git = new Git(repository)) {
+      
+      git.archive()
+      .setFormat("zip")
+      .setOutputStream(out)
+      .setTree(repository.resolve("master"))
+      .call();
+
+//  try {
+//         git.archive()
+//                 .setTree(db.resolve("HEAD"))
+//                 .setOutputStream(out)
+//                 .call();
+//  } finally {
+//         ArchiveCommand.unregisterFormat("tar");
+//  }
+ 
+    }
+    
+		run(env, "./xbuildfile");
+
+		
+
 	}
 
-	private void run(String... args) throws Exception {
+//	private void archive() throws Exception {
+//		java.util.zip.ZipFile zipFile = new ZipFile(file);
+//		try {
+//			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+//			while (entries.hasMoreElements()) {
+//				ZipEntry entry = entries.nextElement();
+//				File entryDestination = new File(outputDir, entry.getName());
+//				if (entry.isDirectory()) {
+//					entryDestination.mkdirs();
+//				} else {
+//					entryDestination.getParentFile().mkdirs();
+//					InputStream in = zipFile.getInputStream(entry);
+//					OutputStream out = new FileOutputStream(entryDestination);
+//					IOUtils.copy(in, out);
+//					IOUtils.closeQuietly(in);
+//					out.close();
+//				}
+//			}
+//		} finally {
+//			zipFile.close();
+//		}
+//	}
+
+	private void run(Map<String, String> env, String... command) throws Exception {
 		log("----------------------------------------------------------------------");
-		log("run", Lists.newArrayList(args));
+		log("run", Lists.newArrayList(command));
 		log("----------------------------------------------------------------------");
 
-		if (new ProcessBuilder(args).redirectError(Redirect.INHERIT).redirectOutput(Redirect.INHERIT).start().waitFor() != 0)
+		ProcessBuilder builder = new ProcessBuilder(command);
+			builder.environment().putAll(env);
+			builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+			builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+
+		if (builder.start().waitFor() != 0)
 			throw new Exception();
 	}
+
+  private List<String> findall(String regex, String s) {
+    List<String> list = new ArrayList<>();
+    Matcher m = Pattern.compile(regex).matcher(s);
+    while (m.find())
+      list.add(m.group(0));
+    return list;
+  }
 
 	private void log(Object... args) {
 		new LogHelper(this).log(args);
---
+++ x
@@ -0,0 +1,3 @@
+#!/bin/sh
+gradle bootrun
+#gradle bootrun --args foo
