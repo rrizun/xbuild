b53f95a26be9228122ef5b1429620a9bdf61cda0
rrizun <rrizun@gmail.com>
2019-08-03 04:50:26 -0700

    touchup

--- src/main/java/xbuild/Posix.java
+++ src/main/java/xbuild/Posix.java
@@ -5,13 +5,13 @@ import java.util.*;
 
 public class Posix {
   /**
-   * returns the set of permissions for the given mode
+   * returns the set of posix file permissions for the given posix mode
    * 
    * @param intMode
    * @return
    */
   public static Set<PosixFilePermission> perms(int intMode) {
-    Set<PosixFilePermission> set = new HashSet<>();
+    Set<PosixFilePermission> set = new TreeSet<>();
     for (int i = 0; i < PosixFilePermission.values().length; i++) {
       if ((intMode & 1) == 1)
         set.add(PosixFilePermission.values()[PosixFilePermission.values().length - i - 1]);
